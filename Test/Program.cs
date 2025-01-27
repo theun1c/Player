ce295e9ef5161a96d93a9e6894d95beddbddaa03
theun1c <belovdima1122@yandex.ru>
2025-01-28 02:39:02 +0300

    dev:test: api connection test

---
+++ Test/Program.cs
@@ -0,0 +1,14 @@
+﻿
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        HttpClient client = new HttpClient();
+
+        HttpResponseMessage response = await client.GetAsync("http://api.anilibria.tv/v3/title?id=9000");
+
+        Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+    }
+}
+
