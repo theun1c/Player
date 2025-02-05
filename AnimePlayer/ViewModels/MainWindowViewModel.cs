51a8eebfbcd44d3daa34d7e654a0fb493a6316bd
theun1c <belovdima1122@yandex.ru>
2025-02-06 00:26:15 +0300

    wor with viewmodel

---
+++ AnimePlayer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using AnimePlayer.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimePlayer.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        private string _Title = "ANIMESHKA";
+
+        public string Title 
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+    }
+}
