31efb939a9d514808a7b3523706436d4c76ca7e6
theun1c <belovdima1122@yandex.ru>
2025-02-18 00:03:00 +0300

    add new branch for backend

---
+++ AnimePlayerBack/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AnimePlayerBack.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
