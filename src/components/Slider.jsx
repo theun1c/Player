518937fbe7b729d38cd40010086e9ba10ad2f398
theun1c <belovdima1122@yandex.ru>
2025-03-03 02:29:33 +0300

    try fetching data

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,32 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Carousel } from "react-bootstrap";
 import animeBanner from '../assets/anime_banner_1.jpg';
 
 const Slider = () => {
+    const [sliderItems, setSliderItems] = useState([]);
+
+    const fetchSliderItems = async () => {
+        try {
+            const response = await fetch('https://localhost:7140/api/Slider');
+            if(!response.ok){
+                throw new Error('network response was not ok');
+            }
+            const data = await response.json();
+            setSliderItems(data);
+        } catch(error){
+            console.error('error fetching slider items', error);
+        }
+    }
+
+    useEffect(() => {
+        fetchSliderItems();
+    }, []);
+
     return (
         <div>
             <Carousel>
-                <Carousel.Item style={{'height': '876px'}}>
+                { sliderItems.map((item) => (
+                    <Carousel.Item style={{'height': '876px'}}>
                     <img src={animeBanner} className="d-block w-100" alt="anime_carousel"/>
                     <Carousel.Caption>
-                        <h3>Anime test slider</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, harum? Mollitia, corporis.</p>
+                        <h3>{item.title}</h3>
+                        <p>{item.description}</p>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item style={{'height': '876px'}}>
-                    <img src={animeBanner} className="d-block w-100" alt="anime_carousel"/>
-                    <Carousel.Caption>
-                        <h3>Anime test slider</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, harum? Mollitia, corporis.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item style={{'height': '876px'}}>
-                    <img src={animeBanner} className="d-block w-100" alt="anime_carousel"/>
-                    <Carousel.Caption>
-                        <h3>Anime test slider</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, harum? Mollitia, corporis.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
+                ))}
             </Carousel>
         </div>
     );
